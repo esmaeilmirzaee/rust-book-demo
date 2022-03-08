a6c1c97cc21851a10fa5a0e27ec8acdef47a1fe8
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-03-08 11:44:59 +0330

    Checking loops

---
+++ ch_03/loops/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+  let mut count = 0;
+  'counting_up: loop {
+    println!("Count: {}", count);
+    let mut remaining = 10;
+
+    loop {
+      println!("remaining {}", remaining);
+      if remaining == 9 {
+        break;
+      }
+      if count == 2 {
+        break 'counting_up;
+      }
+      remaining -= 1;
+    }
+
+    count += 1;
+  }
+  println!("End count = {}", count);
+
+  using_while_loop([10, 20, 30, 40, 50, 60, 70, 80, 90]);
+}
+
+
+fn using_while_loop (arr: [i32; 9]) {
+  let mut index = 0;
+  while index < arr.len() {
+    println!("{} of array is {}", index, arr[index]);
+    index += 1;
+  }
+  println!("Finished printing the array");
+}
