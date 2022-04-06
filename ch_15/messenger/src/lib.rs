97a1216d6af7af96f808980fb52c81a28c5f8fff
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-04-07 02:33:13 +0430

    Access immutable data and mutate using RcCell

--- ch_15/messenger/src/lib.rs
+++ ch_15/messenger/src/lib.rs
@@ -27,7 +27,7 @@ impl<'a, T> LimitTracker<'a, T> where T: Messenger {
         } else if percentage_of_max >= 0.9 {
             self.messenger.send("Urgent warning: You've used up over 90% of your quota")
         } else if percentage_of_max >= 0.75 {
-            self.messenger("Warning: You've used up over 75% of your quota.")
+            self.messenger.send("Warning: You've used up over 75% of your quota.")
         }
     }
 }
@@ -35,22 +35,23 @@ impl<'a, T> LimitTracker<'a, T> where T: Messenger {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
             MockMessenger {
-                sent_messages: vec![],
+                sent_messages: RefCell::new(vec![]),
             }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, msg: &str) {
-            self.sent_messages.push(String::from(msg));
+            self.sent_messages.borrow_mut().push(String::from(msg));
         }
     }
 
@@ -60,6 +61,6 @@ mod tests {
         let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
 
         limit_tracker.set_value(80);
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
